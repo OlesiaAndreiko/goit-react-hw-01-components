91a02cf95e250da54eb64dfe3a7e590921411b46
Olesia Andreiko <olesikmail@gmail.com>
2023-01-16 11:50:29 +0200

    task 1 start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { Profile } from "./Profile/Profile";
+import user from "../data/user.json"
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div
+  style={{
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 40,
+    color: '#010101'
+  }}>
+    <Profile user = {user}/>
+  </div>;
 };
