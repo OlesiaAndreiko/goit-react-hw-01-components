fe1ef6922309b81a227ebd433fd4bfc3fedcb795
Olesia Andreiko <olesikmail@gmail.com>
2023-01-16 16:15:16 +0200

    complete task 1

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,3 +1,18 @@
+import PropTypes from "prop-types";
+import {
+  ProfileUser,
+  ProfileDesc,
+  ProfileAvatar, 
+  UserName, 
+  UserTag,
+  UserLocation,
+  ProfileStats,
+  StatsItem,
+  StatsLabel,
+  StatsQuantity,
+  LastStatsItem,
+} from "./Profile.styled"
+
 export const Profile = ({
   user: {
     username,
@@ -8,31 +23,47 @@ export const Profile = ({
   },
 }) => {
   return (    
-    <div>
-    <div>
-      <img
-        src={avatar}
-        alt={username}
-      />
-      <p>{username}</p>
-      <p>@{tag}</p>
-      <p>{location}</p>
-    </div>
-  
-    <ul>
-      <li>
-        <span>Followers</span>
-        <span>{followers}</span>
-      </li>
-      <li>
-        <span>Views</span>
-        <span>{views}</span>
-      </li>
-      <li>
-        <span>Likes</span>
-        <span>{likes}</span>
-      </li>
-    </ul>
-  </div>
+<ProfileUser>
+<ProfileDesc>
+  <ProfileAvatar
+    src={avatar}
+    alt={"User " + username}
+  />
+  <UserName>{username}</UserName>
+  <UserTag>{"@"+tag}</UserTag>
+  <UserLocation>{location}</UserLocation>
+</ProfileDesc>
+
+<ProfileStats>
+  <StatsItem>
+    <StatsLabel>Followers</StatsLabel>
+    <StatsQuantity>{followers}</StatsQuantity>
+  </StatsItem>
+  <StatsItem>
+    <StatsLabel>Views</StatsLabel>
+    <StatsQuantity>{views}</StatsQuantity>
+  </StatsItem>
+  <LastStatsItem>
+    <StatsLabel>Likes</StatsLabel>
+    <StatsQuantity>{likes}</StatsQuantity>
+  </LastStatsItem>
+</ProfileStats>
+</ProfileUser>
   );
 };
+
+  Profile.propTypes = {
+    user: PropTypes.exact({
+      username: PropTypes.string.isRequired,
+      tag: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      stats: PropTypes.exact({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+      }),
+    }).isRequired,
+};
+
+
